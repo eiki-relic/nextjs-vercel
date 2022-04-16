73934e4fcfc3c4ac126194a2c81e229685501e92
栄木悠也 <eikiyuya@eikiyuyanoMacBook-Pro.local>
2022-04-17 02:54:09 +0900

    本番

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,20 +1,69 @@
+// import { useRouter } from "next/router";
+// import Link from "next/link";
+// import styles from "../../styles/Home.module.css";
+// // ssg
+// export async function getStaticProps({ params }) {
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await req.json();
+//   return {
+//     props: {
+//       product: data,
+//     },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const req = await fetch("http://localhost:3000/products.json");
+//   const data = await req.json();
+//   const paths = data.map((product) => {
+//     return {
+//       params: {
+//         id: product,
+//       },
+//     };
+//   });
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// const Product = ({ product }) => {
+//   const router = useRouter();
+//   const id = router.query.id;
+//   console.log(id);
+//   return (
+//     <div className={styles.container}>
+//       <main className={styles.main}>
+//         <h1>{id}のページです</h1>
+//         <img src={product.image} width="300" height="400" alt="image" />
+//         <p>{product.name}</p>
+//         <br />
+//         <Link href="/products">戻る</Link>
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default Product;
 import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
-// ssg
+
 export async function getStaticProps({ params }) {
+  //jsonファイルのデータを取りに行く。
   const req = await fetch(`http://localhost:3000/${params.id}.json`);
   const data = await req.json();
+
   return {
-    props: {
-      product: data,
-    },
+    props: { product: data },
   };
 }
 
 export async function getStaticPaths() {
-  const req = await fetch(`http://localhost:3000/products.json`);
+  const req = await fetch("http://localhost:3000/products.json");
   const data = await req.json();
+
   const paths = data.map((product) => {
     return {
       params: {
@@ -22,27 +71,41 @@ export async function getStaticPaths() {
       },
     };
   });
+
   return {
     paths,
     fallback: false,
   };
 }
 
-const Products = ({ product }) => {
+// export async function getServerSideProps({ params }) {
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await req.json();
+
+//   return {
+//     props: { product: data },
+//   };
+// }
+
+const Product = ({ product }) => {
   const router = useRouter();
-  const id = router.query.id;
-  console.log(id);
+  const { id } = router.query;
+
+  //   console.log(id);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>{id}のページです</h1>
-        <img src={product.image} width="300" height="400" alt="image" />
+        <img src={product.image} width="300" height="400" />
         <p>{product.name}</p>
         <br />
-        <Link href="/products">戻る</Link>
+        <Link href="/products">
+          <a>商品一覧へ</a>
+        </Link>
       </main>
     </div>
   );
 };
 
-export default Products;
+export default Product;
